73a55560297b2cfe11ae6384cb14987519e28b3c
Jason Keene <jasonkeene@gmail.com>
2015-07-08 17:50:59 -0600

    Stub out project.

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Usage
+
+```bash
+go run exfiltrate.go
+```
+
+To dump all traffic for each agent on exit:
+
+```bash
+go run exfiltrate.go -v
+```
---
+++ exfiltrate.go
@@ -0,0 +1,21 @@
+package main
+
+// gameplan:
+
+// make three connections to gophercon2015.coreos.com 4001
+// record all traffic
+// send channel name on each agent
+// list files on each agent
+// regex output into data_structure
+// determine send list for each agent
+//    rank order documents by secrect_value/size
+//    pack top documents into agents by their capacity
+// send files between agents to match sendlist
+// send files to glenda for each agent
+// send done on each channel
+// stdout result
+// dump traffic
+
+func main() {
+
+}
